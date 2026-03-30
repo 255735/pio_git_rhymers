075f0abc5d710c3f13470afc74cc7518ffa441ed
255735 <255735@edu.p.lodz.pl>
2026-03-30 16:33:39 +0200

    17. Dodanie dokumentacji JavaDoc do FIFORhymer

--- src/main/java/edu/kis/vh/nursery/FIFORhymer.java
+++ src/main/java/edu/kis/vh/nursery/FIFORhymer.java
@@ -1,9 +1,16 @@
 package edu.kis.vh.nursery;
 
+/**
+ *Klasa reprezentujaca wyliczanka w oparciu o kolejke FIFO
+ */
 public class FIFORhymer extends DefaultCountingOutRhymer {
 
     private final DefaultCountingOutRhymer temp = new DefaultCountingOutRhymer();
-    
+
+    /**
+     * Zwraca i usuwa najstarszy element z wyliczanki (zgodnie z zasadą FIFO).
+     * * @return wartość pierwszego dodanego elementu lub wartość domyślną, gdy wyliczanka jest pusta
+     */
     public int countOut() {
         while (!callCheck())
             temp.countIn(super.countOut());
